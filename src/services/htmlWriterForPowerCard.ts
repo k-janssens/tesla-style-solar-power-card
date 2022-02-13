a41b00aba5c341550d46b0980579d0bc3a969da3
kristof.janssens@mirabilis.be <kristof.janssens@mirabilis.be>
2022-02-13 15:29:07 +0100

    Update

--- src/services/htmlWriterForPowerCard.ts
+++ src/services/htmlWriterForPowerCard.ts
@@ -52,7 +52,7 @@ export class HtmlWriterForPowerCard {
           class="acc_text"
           style="font-size:${3 * this.pxRate + 'px'}; margin-top:${-0.5 * this.pxRate + 'px'}; width: ${10 * this.pxRate + 'px'}"
         >
-          ${mainValue} <br />
+          ${mainValue} --<br />
           ${mainUnitOfMeasurement}
         </div>
       </div>
